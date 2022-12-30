1d5c8e5c87f7c832d98d7b89ebb4228eaa479d7b
alof04 <hpclafonso@gmail.com>
2022-12-30 14:01:04 +0000

    .

---
+++ Projeto ITW/pie.js
@@ -0,0 +1,50 @@
+
+        var Counter1 = [];
+        var Medalhas = [];
+        var res = document.location.href.split('?')[1];
+        var yes = res.replace("id=", "");
+
+google.charts.load('current', { 'packages': ['corechart'] }).then(function () {
+
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://192.168.160.58/Olympics/api/Games/FullDetails?id=' + yes,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        success: function (data, status, xhr) {
+
+            Medalhas.push((data.Medals).map(({ MedalName }) => MedalName));
+            Counter1.push((data.Medals).map(({ Counter }) => Counter));
+            const ola1 = parseInt(Counter1[0][0]);
+            const ola2 = parseInt(Counter1[0][1]);
+            const ola3 = parseInt(Counter1[0][2]);
+            console.log(Medalhas);
+            console.log(Counter1);
+
+            function drawChart(Counter1, Medalhas) {
+                var data = new google.visualization.DataTable();
+                console.log(Counter1);
+                data.addColumn('string', 'Medalha');
+                data.addColumn('number', 'Quantidade');
+                data.addRows([
+                    ['Gold', ola1],
+                    ['Silver', ola2],
+                    ['Bronze', ola3]
+                ]);
+                var options = {
+                    legend: {color:'white'},
+                    title: 'Medals atru',
+                    backgroundColor: '#1F2022',
+                    colors: ['#FAC213', '#AAAAAA', '#A64B2A']
+                };
+                var chart = new google.visualization.PieChart(document.getElementById('pie'));
+                chart.draw(data, options);
+            }
+            google.charts.setOnLoadCallback(drawChart);
+
+
+        }
+    });
+});
